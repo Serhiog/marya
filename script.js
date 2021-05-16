4b4be3b6d91074ea676fa8707cf3909080b0ad6b
Сергей Грищенков <grishenkov.sergei@gmail.com>
2021-05-16 21:04:12 +0300

    4

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const searchBtn = document.querySelector(
+  ".header__list-user-item-link--search"
+);
+const searchPanel = document.querySelector(".header__search");
 
+const list = document.querySelector(".header__list");
+
+searchBtn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  searchPanel.classList.toggle("header__search--hidden");
+  searchBtn.classList.toggle("header__list-user-item-link--search--active");
+  list.classList.toggle("header__list--active");
+});
